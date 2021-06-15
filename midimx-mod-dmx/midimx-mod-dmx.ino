1e9342edc05c9c2eb342608e39da732812c7481d
NikolayRag <nikolayr@mail.ru>
2021-06-15 03:39:36 +0300

    add DMX together with i2c

--- midimx-mod-dmx/midimx-mod-dmx.ino
+++ midimx-mod-dmx/midimx-mod-dmx.ino
@@ -11,6 +11,17 @@ Shield used is Iskra RS485, connected to:
     out B -> XLR 2
     out G -> XLR 1
 */
+#include <DMXSerial.h> //485 over Serial, https://github.com/mathertel/DMXSerial
+
+int devChannel = 1;
+int devMode =   devChannel +0;
+int devLamp =   devChannel +3;
+int devRed =    devChannel +4;
+int devGreen =  devChannel +5;
+int devBlue =   devChannel +6;
+int devWhite =  devChannel +7;
+
+
 
 #include <Wire.h>
 
@@ -23,12 +34,20 @@ void i2cRequestCB() {
 
 void i2cRecieveCB(int bytes) {
   Serial.print("in ");
+  byte c;
   while (Wire.available()) {
-    byte c = Wire.read();
+    c = Wire.read();
     Serial.print(c);
     Serial.print(',');
   }
   Serial.print("\n");
+
+
+  DMXSerial.write(devRed, c*2);
+  DMXSerial.write(devGreen, c*2);
+  DMXSerial.write(devBlue, c*2);
+  DMXSerial.write(devWhite, 0);
+  DMXSerial.write(devLamp, 255);
 }
 
 
@@ -39,6 +58,12 @@ void setup() {
   Wire.begin(0x01);
   Wire.onReceive(i2cRecieveCB);
   Wire.onRequest(i2cRequestCB);
+
+
+  DMXSerial.init(DMXController);
+
+  DMXSerial.write(devMode, 0);
+
 
   Serial.print("Init\n");
 }
