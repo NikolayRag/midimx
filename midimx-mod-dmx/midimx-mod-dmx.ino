657480aa67b5fe771ecb089bbf5563d4c963250f
NikolayRag <nikolayr@mail.ru>
2021-06-15 03:37:15 +0300

    clean DMX code out

--- midimx-mod-dmx/midimx-mod-dmx.ino
+++ midimx-mod-dmx/midimx-mod-dmx.ino
@@ -43,39 +43,6 @@ void setup() {
   Serial.print("Init\n");
 }
 
-void loop() {
-}
-
-/*
-#include <DMXSerial.h> //485 over Serial, https://github.com/mathertel/DMXSerial
-
-
-int devChannel = 1;
-int devMode =   devChannel +0;
-int devLamp =   devChannel +3;
-int devRed =    devChannel +4;
-int devGreen =  devChannel +5;
-int devBlue =   devChannel +6;
-int devWhite =  devChannel +7;
-
-int dmxV = 0;
-
-void setup() {
-  DMXSerial.init(DMXController);
-
-  DMXSerial.write(devMode, 0);
-  DMXSerial.write(devLamp, 255);
-}
-
 
 void loop() {
-  DMXSerial.write(devRed, 127);
-  DMXSerial.write(devGreen, dmxV%256);
-  DMXSerial.write(devBlue, 255-(dmxV%256) );
-  DMXSerial.write(devWhite, 0);
-
-  dmxV++;
-  
-  delay(5);
 }
-*/
